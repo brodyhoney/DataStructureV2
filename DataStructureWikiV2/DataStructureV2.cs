9a2004eb27f18379797349467da649bb2fc9ad68
BRODY HONEY <30043475@tafe.wa.edu.au>
2022-06-01 13:33:19 +0800

    Added trace code to ValidName, Delete and Edit

--- DataStructureWikiV2/DataStructureV2.cs
+++ DataStructureWikiV2/DataStructureV2.cs
@@ -1,5 +1,6 @@
+using System.IO;
 using System.Text;
-
+using System.Diagnostics;
 namespace DataStructureWikiV2
 {
     public partial class DataStructureWikiV2 : Form
@@ -58,13 +59,26 @@ namespace DataStructureWikiV2
         }// 6.14 Save binary file
         private void Btn_Del_Click(object sender, EventArgs e)
         { 
+            TextWriterTraceListener tr = new TextWriterTraceListener(System.Console.Out);
+            Trace.Listeners.Add(tr);
+            Trace.WriteLine("START: Debugging Delete button");
             if (listView_Wiki.SelectedItems.Count > 0)
             {
+                Trace.Indent();
+                Trace.WriteLine("Delete clicked and item is selected");
                 DialogResult dr = MessageBox.Show("Are you sure you want to remove this item?", "Removal Confirmation",
                 MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (dr == DialogResult.Yes)
                 {
+                    Trace.WriteLine("User prompted by message box");
+                    Trace.WriteLine("ANSWER: " + dr);
+                    Trace.WriteLineIf(dr == DialogResult.No, "ANSWER: " + dr + ", operation cancelled");
+                    Trace.WriteLineIf(dr == DialogResult.Yes, "Item to delete: " + Wiki[listView_Wiki.SelectedIndices[0]].Name);
                     Wiki.Remove(Wiki[listView_Wiki.SelectedIndices[0]]); // Removes selected item
+                    Trace.WriteLineIf(dr == DialogResult.Yes, "Selected item removed, method working correctly");
+                    Trace.Unindent();
+                    Trace.WriteLine("END: Debugging Delete button complete");
+                    Trace.Flush();
                     DisplayWiki(); // Updates list view
                     ClearAttributes();
                     toolStripStatus.Text = "Item removed";
@@ -72,34 +86,55 @@ namespace DataStructureWikiV2
             }
             else
             {
+                Trace.WriteLineIf(!(listView_Wiki.SelectedItems.Count > 0), "No item selected, nothing to delete");
+                Trace.WriteLine("END: Debugging Delete button complete");
+                Trace.Flush();
                 toolStripStatus.Text = "Nothing selected";
             }
+            
 
         }// 6.7 Deleted selected record from the list
         private void Btn_Edit_Click(object sender, EventArgs e)
         {
+            TextWriterTraceListener tr = new TextWriterTraceListener(System.Console.Out);
+            Trace.Listeners.Add(tr);
+            Trace.WriteLine("START: Debugging Edit button");
+            Trace.Indent();
             if (listView_Wiki.SelectedItems.Count > 0)
             {
                 if (!AnyAttributesEmpty()) // Check if any of the attributes are empty
                 {
                     if (!ValidName(textBox_Name.Text)) // Check that a duplicate is not being added
                     {
+                        Trace.WriteLine("Item to edit: " + Wiki[listView_Wiki.SelectedIndices[0]].Name);
                         Wiki[listView_Wiki.SelectedIndices[0]].Name = textBox_Name.Text;
                         Wiki[listView_Wiki.SelectedIndices[0]].Category = comboBox_Category.Text;
                         Wiki[listView_Wiki.SelectedIndices[0]].Structure = GetRadioButtonOutput();
                         Wiki[listView_Wiki.SelectedIndices[0]].Definition = textBox_Definition.Text;
+                        Trace.WriteLine("Item "+textBox_Name.Text+" successfully edited, method working correctly");
+                        Trace.Unindent();
+                        Trace.WriteLine("END: Debugging Edit button complete");
+                        Trace.Flush();
                         DisplayWiki();
                         ClearAttributes();
                         toolStripStatus.Text = "Item edited";
                     }
                     else
                     {
+                        Trace.WriteLineIf(!ValidName(textBox_Name.Text), "Can't edit, duplicate detected");
+                        Trace.Unindent();
+                        Trace.WriteLine("END: Debugging Edit button complete");
+                        Trace.Flush();
                         toolStripStatus.Text = "That structure is already in the list";
                         ClearAttributes();
                     }
                 }
                 else
                 {
+                    Trace.WriteLineIf(!AnyAttributesEmpty(), "Can't edit, one or more attributes are empty");
+                    Trace.Unindent();
+                    Trace.WriteLine("END: Debugging Edit button complete");
+                    Trace.Flush();
                     toolStripStatus.Text = "One or more attributes are empty";
                     ClearAttributes();
                 }
@@ -107,6 +142,10 @@ namespace DataStructureWikiV2
             }
             else
             {
+                Trace.WriteLineIf(!(listView_Wiki.SelectedItems.Count > 0), "No item selected, nothing to edit");
+                Trace.Unindent();
+                Trace.WriteLine("END: Debugging Edit button complete");
+                Trace.Flush();
                 toolStripStatus.Text = "Nothing selected";
             }
         }// 6.8 Edit selected record 
@@ -202,14 +241,33 @@ namespace DataStructureWikiV2
         }// 6.11 Event to select record from list and display it's attributes
         private bool ValidName(string n)
         {
+            /*TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
+            Trace.Listeners.Add(tr1);
+            Trace.WriteLine("START: Debugging ValidName() Method");
+            Trace.Indent();
+            Trace.WriteLine("Testing for duplicates of '" + n + "'.");*/
+            
             if (Wiki.Exists(info => info.Name == n))
             {
+                /*Trace.WriteLineIf(Wiki.Exists(info => info.Name == n), n+" has a duplicate");
+                Trace.WriteLine("Duplicates prevented, method functioning correctly");
+                Trace.Unindent();
+                Trace.WriteLine("END: Debugging of ValidName() Method complete");
+                Trace.Flush();
+                Console.ReadLine();*/
                 return true;
             }
             else
             {
+                /*Trace.WriteLineIf(!(Wiki.Exists(info => info.Name == n)), n+" has no duplicate");
+                Trace.WriteLine("No duplicates detected, method functioning correctly");
+                Trace.Unindent();
+                Trace.WriteLine("END: Debugging of ValidName() Method complete");
+                Trace.Flush();
+                Console.ReadLine();*/
                 return false;
             }
+            
         }// 6.5 Method that uses List.Exists to determine if a duplicate exists
         private void ClearAttributes()
         {
